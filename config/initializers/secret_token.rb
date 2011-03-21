a34391eaf44b2b8149a67009430be02298b837c2
Igor P. Leroy <ip.leroy@gmail.com>
2011-03-21 16:58:26 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OcapiChallenge::Application.config.secret_token = '47559233ba54af0e59a1f35a7df6324e59f08aacd958900a113b9efc4077b5c3c234845b1d2c494792a03047e8d7cf38978ca7e61bffee924c2787aa85e2055c'
